08e236906ed94c5502ce5ade471c4ca368514311
dvjpa <paulotta0208@outlook.com>
2025-12-06 13:42:57 -0300

    corrigindo erros

--- usuarios/views.py
+++ usuarios/views.py
@@ -96,17 +96,10 @@ def editar_perfil(request):
         form = UsuarioUpdateForm(request.POST, request.FILES, instance=usuario)
         if form.is_valid():
             form.save()
-<<<<<<< HEAD
-            return redirect("usuarios:perfil")  # <- aqui
+            return redirect("usuarios:perfil")
     else:
         form = UsuarioUpdateForm(instance=usuario)
     
-=======
-            return redirect("usuarios:perfil")  # << CORRETO
-    else:
-        form = UsuarioUpdateForm(instance=usuario)
-
->>>>>>> 57b213e71e33c2acc0655d7f5a06574a71f593fb
     return render(request, "usuarios/editar_perfil.html", {
         "titulo": "Editar Perfil",
         "form": form
